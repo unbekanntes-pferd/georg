76fb496ce82fd5e0f196bf5b30b304c3e3d4d644
Octavio Simone <o.simone@dracoon.com>
2024-05-15 16:48:41 +0200

    feat: initial parsing of xlsx file

--- src-tauri/src/parse/models.rs
+++ src-tauri/src/parse/models.rs
@@ -1,37 +1,81 @@
-use chrono::{DateTime, NaiveDate, Utc};
+use std::path::PathBuf;
+
+use chrono::NaiveDate;
 use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Candidate {
+    #[serde(rename = "Name")]
     pub name: String,
+    #[serde(rename = "Ort")]
     pub location: String,
-    pub qualification: String,
+    #[serde(rename = "Qualif.")]
+    pub qualification: Option<String>,
+    #[serde(rename = "Stundenumfang")]
     pub hours: Option<String>,
+    #[serde(rename = "Mobilität")]
     pub mobility: Option<String>,
-    pub received_at: DateTime<Utc>,
+    #[serde(rename = "Eingang")]
+    pub received_at: Option<String>,
+    #[serde(rename = "Bemerkungen")]
     pub notes: Option<String>,
+    #[serde(rename = "Geplanter Beginn")]
     pub start_note: Option<String>,
+    #[serde(rename = "Personalbogen")]
     pub sent_documents: Option<String>,
+    #[serde(rename = "Checkliste komplett")]
     pub completed_checklist: Option<String>,
+    #[serde(rename = "Masernschutz")]
     pub vaccination_stat: Option<String>,
+    #[serde(rename = "Führungszeugnis")]
     pub certification_state: Option<String>,
+    #[serde(rename = "Personalbogen")]
     pub personal_documentation: Option<String>,
+    #[serde(rename = "Geplantes Kind")]
     pub planned_child: Option<String>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct ChildCareRequest {
+    #[serde(rename = "Einrichtung")]
     pub institution: String,
+    #[serde(rename = "Ort")]
     pub location: String,
+    #[serde(rename = "Klasse")]
     pub grade: String,
+    #[serde(rename = "Stunden")]
     pub hours: String,
+    #[serde(rename = "Diagnose")]
     pub diagnosis: Option<String>,
+    #[serde(rename = "Ansprechpartner*in")]
     pub contact: String,
-    pub received_at: NaiveDate,
+    #[serde(rename = "Datum")]
+    pub received_at: Option<String>,
+    #[serde(rename = "Bemerkung")]
     pub notes: Option<String>,
 }
 
+#[derive(Debug, Clone, Deserialize)]
 pub struct CandidateRequests {
     pub candidates: Vec<Candidate>,
     pub child_care_requests: Vec<ChildCareRequest>,
+}
+
+impl CandidateRequests {
+    pub fn new(candidates: Vec<Candidate>, child_care_requests: Vec<ChildCareRequest>) -> Self {
+        Self {
+            candidates,
+            child_care_requests,
+        }
+    }
+}
+
+pub struct ImportFiles {
+    pub candidates_file: PathBuf,
+}
+
+impl ImportFiles {
+    pub fn new(candidates_file: PathBuf) -> Self {
+        Self { candidates_file }
+    }
 }
