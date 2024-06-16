3d6a9b12a063e9bea2486223e83f286b79e3802d
Ralf Böhler <ralf.boehler.90@gmail.com>
2024-06-16 15:43:33 +0200

    chore: renaming route and tabs

--- src/lib/components/LeftSideBar.svelte
+++ src/lib/components/LeftSideBar.svelte
@@ -4,6 +4,6 @@
 
 <div class="w-48 bg-surface-800 h-full">
 	<button on:click={() => goto('candidates')} class="btn">Bewerber</button>
-	<button on:click={() => goto('school-assistant')} class="btn">Schulbegleitung</button>
+	<button on:click={() => goto('school-assistance')} class="btn">Schulbegleitung</button>
 	<button on:click={() => goto('settings')} class="btn">Einstellungen</button>
 </div>
