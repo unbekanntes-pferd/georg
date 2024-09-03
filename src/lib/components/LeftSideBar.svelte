26e5c186e4cf3ab070024dc5192a132ea85c8798
Ralf Böhler <ralf.boehler.90@gmail.com>
2024-09-03 19:56:40 +0200

    style: underline current page in left sidebar navigation

--- src/lib/components/LeftSideBar.svelte
+++ src/lib/components/LeftSideBar.svelte
@@ -1,32 +1,63 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { Command, State, excelPathCommandStore, type ExcelPathCommandStore } from '$lib/stores/path';
-
-	
+	import { page } from '$app/stores';
+	import {
+		Command,
+		State,
+		excelPathCommandStore,
+		type ExcelPathCommandStore
+	} from '$lib/stores/path';
 
 	$: disabledCandidates = disableCandidates($excelPathCommandStore);
 	$: disabledSchoolAssistants = disableSchoolAssistants($excelPathCommandStore);
 
 	function disableCandidates(store: ExcelPathCommandStore) {
-		let candidatesEntry = store.directories.find((directory) => directory.command === Command.CandidatesAndChildCareRequests);
+		let candidatesEntry = store.directories.find(
+			(directory) => directory.command === Command.CandidatesAndChildCareRequests
+		);
 		if (candidatesEntry?.path === '' || candidatesEntry?.state !== State.Ok) {
 			return true;
-		} 
+		}
 		return false;
 	}
 
 	function disableSchoolAssistants(store: ExcelPathCommandStore) {
-		let schoolAssistantsEntry = $excelPathCommandStore.directories.find((directory) => directory.command === Command.SchoolAssistants);
+		let schoolAssistantsEntry = $excelPathCommandStore.directories.find(
+			(directory) => directory.command === Command.SchoolAssistants
+		);
 		if (schoolAssistantsEntry?.path === '' || schoolAssistantsEntry?.state !== State.Ok) {
 			return true;
-		} 
+		}
 		return false;
 	}
 
+	$: isCandidatesRoute = $page.url.pathname === '/candidates';
+	$: isSchoolAssistantsRoute = $page.url.pathname === '/school-assistance';
+	$: isSettingsRoute = $page.url.pathname === '/settings';
 </script>
 
 <div class="flex flex-col w-48 h-full">
-	<button disabled={disabledCandidates} on:click={() => goto('candidates')} class="btn">Bewerber</button>
-	<button disabled={disabledSchoolAssistants} on:click={() => goto('school-assistance')} class="btn">Schulbegleitung</button>
-	<button on:click={() => goto('settings')} class="btn mt-auto">Einstellungen</button>
+	<button
+		disabled={disabledCandidates}
+		on:click={() => goto('candidates')}
+		class="btn {isCandidatesRoute ? 'active' : ''}"
+	>
+		Bewerber
+	</button>
+	<button
+		disabled={disabledSchoolAssistants}
+		on:click={() => goto('school-assistance')}
+		class="btn {isSchoolAssistantsRoute ? 'active' : ''}"
+	>
+		Schulbegleitung
+	</button>
+	<button on:click={() => goto('settings')} class="btn mt-auto {isSettingsRoute ? 'active' : ''}">
+		Einstellungen
+	</button>
 </div>
+
+<style>
+	.active {
+		text-decoration: underline;
+	}
+</style>
